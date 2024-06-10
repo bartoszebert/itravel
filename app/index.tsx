01ca0575a59aa322f0311a3cc80a09a17796ce98
Bartosz Ebert <bartoszebert@gmail.com>
2024-06-10 14:26:43 +0200

    configure fonts and colors for tailwind

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text className="font-pbold">Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,43 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./App.{js,jsx,ts,tsx}",
-    "./app/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./App.{js,jsx,ts,tsx}", "./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#292929",
+        secondary: {
+          DEFAULT: "#5e54ce",
+          100: "#e2e0f9",
+          200: "#c5c1f2",
+          300: "#a7a1eb",
+          400: "#8982e4",
+          500: "#6c62dd",
+          600: "#564dcc",
+          700: "#423a99",
+          800: "#2f2766",
+          900: "#1c1533",
+        },
+        black: {
+          DEFAULT: "#000",
+          100: "#1E1E2D",
+          200: "#232533",
+        },
+        gray: {
+          DEFAULT: "#CDCDE0",
+        },
+      },
+      fontFamily: {
+        pthin: ["Poppins-Thin", "sans-serif"],
+        pextralight: ["Poppins-ExtraLight", "sans-serif"],
+        plight: ["Poppins-Light", "sans-serif"],
+        pregular: ["Poppins-Regular", "sans-serif"],
+        pmedium: ["Poppins-Medium", "sans-serif"],
+        psemibold: ["Poppins-SemiBold", "sans-serif"],
+        pbold: ["Poppins-Bold", "sans-serif"],
+        pextrabold: ["Poppins-ExtraBold", "sans-serif"],
+        pblack: ["Poppins-Black", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
