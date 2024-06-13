9f4895d89382e41b9e9a5682cfdb9f50aa955a62
Bartosz Ebert <bartoszebert@gmail.com>
2024-06-13 10:42:32 +0200

    reorganise folder structure - app as routing, screens as a features

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,52 +1,7 @@
-import CustomButton from "@/components/ui/CustomButton";
-import { LinearGradient } from "expo-linear-gradient";
-import { ScrollView, Text, View, Image } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { images } from "@/constants";
-import { Redirect, router } from "expo-router";
-import { useGlobalContext } from "@/context/GlobalProvider";
+import Welcome from "@/components/screens/Welcome";
 
-export default function Index() {
-  const { isLoading, isLogged } = useGlobalContext();
-
-  if (!isLoading && isLogged) return <Redirect href="/home" />;
-  
+export default function IndexPage() {
   return (
-    <View className="bg-secondary">
-      <LinearGradient
-        className="absolute top-0 left-0 w-full h-full"
-        colors={["transparent", "rgba(255,255,255,0.3)"]}
-      />
-      <SafeAreaView className=" h-full">
-        <ScrollView>
-          <View className="w-full justify-around items-center px-8 min-h-[85vh]">
-            <Image
-              source={images.logo}
-              className="w-[200px] h-[100px]"
-              resizeMode="contain"
-            />
-            <Image
-              source={images.travel}
-              className="w-[100px] h-[100px]"
-              resizeMode="contain"
-            />
-            <View>
-              <Text className="font-pbold text-3xl color-white">
-                Let's explore the world together!
-              </Text>
-              <Text className="font-pregular text-sm color-white mt-3">
-                Plan your perfect trip together. Coordinate with friends, vote
-                on destinations, and create shared itineraries effortlessly.
-              </Text>
-              <CustomButton
-                title="Get Started"
-                handlePress={() => router.push("/sign-in")}
-                containerStyles="mt-8"
-              />
-            </View>
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </View>
+    <Welcome />
   );
 }
