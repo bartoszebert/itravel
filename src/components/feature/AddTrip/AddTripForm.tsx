735453f181bb992d64e388916c4fef91a66c0019
Bartosz Ebert <bartoszebert@gmail.com>
2024-06-18 10:50:16 +0200

    refactor(AddTravel): separate the concerns

--- src/components/feature/AddTrip/AddTripForm.tsx
+++ src/components/feature/AddTrip/AddTripForm.tsx
@@ -1,80 +1,25 @@
-import useAddTravel from "@/api/travelList/useAddTravel";
-import useGetLocations from "@/api/useGetLocations";
 import CustomButton from "@/components/ui/CustomButton";
 import DatePickerField from "@/components/ui/DatePickerField";
 import FormField from "@/components/ui/FormField";
-import { useGlobalContext } from "@/context/GlobalProvider";
-import useDebounce from "@/hooks/useDebounce";
-import { IAddTripForm } from "@/interfaces/IAddTripForm";
-import { defaultTrip } from "@/utils/defaultTrip";
+import useAddTripForm from "@/hooks/itravel/useAddTripForm";
 import { router } from "expo-router";
-import { useEffect, useState } from "react";
-import { Keyboard, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import LocationListItem from "../../ui/LocationListItem";
 import ShowLoadedPhoto from "./ShowLoadedPhoto";
 
 const AddTripForm = () => {
-  const { user } = useGlobalContext();
-
-  const [destination, setDestination] = useState<string>("");
-  const [locations, setLocations] = useState<any[]>([]);
-  const [form, setForm] = useState<IAddTripForm>(defaultTrip);
-
-  const debouncedDestination = useDebounce(destination, 500);
-
-  const { refetch, data } = useGetLocations(debouncedDestination);
-
-  const handleSetPhoto = (photoUrl: string) => {
-    setForm((prev) => ({
-      ...prev,
-      photo: photoUrl,
-    }));
-  };
-
-  const handleSetStartDate = (date: Date | undefined) => {
-    setForm((prev) => {
-      const newStartDate = date || prev.startDate;
-      return {
-        ...prev,
-        startDate: newStartDate,
-        endDate: prev.endDate < newStartDate ? newStartDate : prev.endDate,
-      };
-    });
-  };
-
-  const handleSetEndDate = (date: Date | undefined) => {
-    setForm((prev) => {
-      const newEndDate = date || prev.endDate;
-      return {
-        ...prev,
-        endDate: newEndDate,
-        startDate: prev.startDate > newEndDate ? newEndDate : prev.startDate,
-      };
-    });
-  };
-
-  const submit = async () => {
-    if (!form.name || !form.destination || !user) return;
-
-    const query = { ...form, owner: user.accountId };
-    const result = await useAddTravel(query);
-
-    if (result) {
-      router.push("/trips");
-    } else {
-      console.error("Failed to add the trip.");
-    }
-  };
-
-  useEffect(() => {
-    setLocations([]);
-    refetch(debouncedDestination);
-  }, [debouncedDestination]);
-
-  useEffect(() => {
-    if (!data) return;
-    setLocations(data);
-  }, [data]);
+  const {
+    destination,
+    setDestination,
+    locations,
+    form,
+    setForm,
+    handleSetPhoto,
+    handleSetStartDate,
+    handleSetEndDate,
+    handleSelectLocation,
+    submit,
+  } = useAddTripForm();
 
   return (
     <View className="pt-20 px-10 h-full justify-start">
@@ -100,14 +45,7 @@ const AddTripForm = () => {
             <LocationListItem
               key={item.place_id}
               item={item}
-              onSelect={() => {
-                setForm((prev) => ({
-                  ...prev,
-                  destination: item.formatted,
-                }));
-                setLocations([]);
-                Keyboard.dismiss();
-              }}
+              onSelect={() => handleSelectLocation(item)}
             />
           ))}
       </View>
---
+++ src/hooks/itravel/useAddTripForm.ts
@@ -0,0 +1,97 @@
+import useAddTravel from "@/api/travelList/useAddTravel";
+import useGetLocations from "@/api/useGetLocations";
+import { useGlobalContext } from "@/context/GlobalProvider";
+import { IAddTripForm } from "@/interfaces/IAddTripForm";
+import { defaultTrip } from "@/utils/defaultTrip";
+import { router } from "expo-router";
+import { useEffect, useState } from "react";
+import { Keyboard } from "react-native";
+import useDebounce from "../useDebounce";
+
+const useAddTripForm = () => {
+  const { user } = useGlobalContext();
+
+  const [destination, setDestination] = useState<string>("");
+  const [locations, setLocations] = useState<any[]>([]);
+  const [form, setForm] = useState<IAddTripForm>(defaultTrip);
+
+  const debouncedDestination = useDebounce(destination, 500);
+
+  const { refetch, data } = useGetLocations(debouncedDestination);
+
+  const handleSelectLocation = (item: any) => {
+    setForm((prev) => ({
+      ...prev,
+      destination: item.formatted,
+    }));
+    setLocations([]);
+    Keyboard.dismiss();
+  };
+
+  const handleSetPhoto = (photoUrl: string) => {
+    setForm((prev) => ({
+      ...prev,
+      photo: photoUrl,
+    }));
+  };
+
+  const handleSetStartDate = (date: Date | undefined) => {
+    setForm((prev) => {
+      const newStartDate = date || prev.startDate;
+      return {
+        ...prev,
+        startDate: newStartDate,
+        endDate: prev.endDate < newStartDate ? newStartDate : prev.endDate,
+      };
+    });
+  };
+
+  const handleSetEndDate = (date: Date | undefined) => {
+    setForm((prev) => {
+      const newEndDate = date || prev.endDate;
+      return {
+        ...prev,
+        endDate: newEndDate,
+        startDate: prev.startDate > newEndDate ? newEndDate : prev.startDate,
+      };
+    });
+  };
+
+  const submit = async () => {
+    if (!form.name || !form.destination || !user) return;
+
+    const query = { ...form, owner: user.accountId };
+    const result = await useAddTravel(query);
+
+    if (result) {
+      router.push("/trips");
+    } else {
+      console.error("Failed to add the trip.");
+    }
+  };
+
+  useEffect(() => {
+    setLocations([]);
+    refetch(debouncedDestination);
+  }, [debouncedDestination]);
+
+  useEffect(() => {
+    if (!data) return;
+    setLocations(data);
+  }, [data]);
+
+  return {
+    destination,
+    setDestination,
+    locations,
+    form,
+    setForm,
+    handleSetPhoto,
+    handleSetStartDate,
+    handleSetEndDate,
+    handleSelectLocation,
+    submit,
+  };
+};
+
+export default useAddTripForm;
